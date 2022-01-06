0903808b5d9d92c0bd8e082c1298c52cdb65ffb2
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2022-01-06 18:31:56 -0300

    Update login.js

--- monitora_tcc/scripts/login.js
+++ monitora_tcc/scripts/login.js
@@ -4,11 +4,19 @@
     const inputLogin = document.querySelector('[data-input-cpf]');
 
     form.addEventListener("submit", e => {
+
+        var url;
+
         e.preventDefault();
-        if (inputLogin.value.toLowerCase() == "professor")
-            window.location.href = '/monitora_tcc/pages/professor.html';
-        else if (inputLogin.value.toLowerCase() == "aluno") 
-            window.location.href = '/monitora_tcc/pages/aluno.html';
+        
+        if (inputLogin.value.toLowerCase() == "1") {
+            url = window.location.href.replace("login", "professor");
+            window.location.href = url;
+        }
+        else if (inputLogin.value.toLowerCase() == "2") { 
+            url = window.location.href.replace("login", "aluno");
+            window.location.href = url;
+        }
         else
             window.alert("Usuário não cadastrado.");
     });
